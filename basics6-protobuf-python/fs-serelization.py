2d9a494bd4d6b064b67ced3727857d9ef98c6ccd
lapeko <lapeko88@gmail.com>
2024-09-03 23:38:12 +0200

    json serialize and deserialize

--- basics6-protobuf-python/fs-serelization.py
+++ basics6-protobuf-python/fs-serelization.py
@@ -1,27 +1,39 @@
+from google.protobuf.json_format import MessageToJson, Parse
 import proto.simple_pb2 as simple
 
 
-FILE_NAME = "simple.bin"
+FILE_NAME = "simple"
+BIN_EXT = "bin"
+JSON_EXT = "json"
 
 
-def serialize_and_save(proto_obj: simple.Simple):
-    with open(FILE_NAME, "wb") as f:
-        f.write(proto_obj.SerializeToString())
+def serialize_bin(message: simple.Simple):
+    with open(f"{FILE_NAME}.{BIN_EXT}", "wb") as f:
+        f.write(message.SerializeToString())
 
 
-def deserialize_from_file():
-    s = simple.Simple()
-    try:
-        with open(FILE_NAME, "rb") as f:
-            s.ParseFromString(f.read())
-            return s
-    except IOError:
-        print("Could not open file: " + FILE_NAME)
+def deserialize_bin():
+    with open(f"{FILE_NAME}.{BIN_EXT}", "rb") as f:
+        s = simple.Simple()
+        s.ParseFromString(f.read())
+        return s
+
+
+def serialize_json(message: simple.Simple):
+    with open(f"{FILE_NAME}.{JSON_EXT}", "w") as f:
+        f.write(MessageToJson(message))
+
+
+def deserialize_json():
+    with open(f"{FILE_NAME}.{JSON_EXT}", "r") as f:
+        return Parse(f.read(), simple.Simple())
 
 
 if __name__ == "__main__":
     smpl = simple.Simple(id=2, is_simple=True, name="Some name", numbers=[1, 2, 3, 5])
-    serialize_and_save(smpl)
-    print(deserialize_from_file())
-
-
+    serialize_bin(smpl)
+    smpl = deserialize_bin()
+    print(smpl)
+    serialize_json(smpl)
+    smpl = deserialize_json()
+    print(smpl)
